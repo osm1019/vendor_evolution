678eb97900ce1e4b4df6e6ffb5f50f405bf51707
Oscar <osm1019@gmail.com>
2024-04-27 11:04:03 -0700

    Rework FaceUnlock call

--- config/evolution.mk
+++ config/evolution.mk
@@ -7,17 +7,20 @@ PRODUCT_PACKAGES += \
     Updater
 endif
 
-ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
-PRODUCT_PACKAGES += \
-    FaceUnlock
+# Face Unlock
 
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    ParanoidSense
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.face.sense_service=true
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
-
+endif
 # Cloned app exemption
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-evolution-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-evolution-product.xml
